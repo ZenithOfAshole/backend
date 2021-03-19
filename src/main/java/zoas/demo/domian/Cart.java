52caa0e2bb26e7ff2981938aa0ea9e0b5e28d2d2
xonmin <han51361@naver.com>
2021-03-19 15:05:24 +0900

    03.19 카트아이템 엔티티 생성

--- src/main/java/zoas/demo/domian/Cart.java
+++ src/main/java/zoas/demo/domian/Cart.java
@@ -3,6 +3,7 @@ package zoas.demo.domian;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.ibatis.annotations.One;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -23,10 +24,37 @@ public class Cart {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
-    @JoinColumn(name = "item_id")
-    private List<Item> items= new ArrayList<>();
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL) // cart를 persist 해도 cartItem에 persist
+    private List<CartItem> cartItems = new ArrayList<>();
+
 
     @Column(name = "cart_quantity")
     private int quantity; //장바구니 내 주문수량
+
+
+    //연관 관게 메서드
+    public void setUser(User user){
+        this.user = user;
+        user.setCart(this);
+    }
+    public void addCartItem(CartItem cartItem){
+        cartItems.add(cartItem);
+        cartItem.setCart(this);
+    }
+
+
+    // 카트 생성 메소드
+    public static Cart createCart(User user,CartItem... cartItems){
+        Cart cart = new Cart();
+        cart.setUser(user);
+
+        for(CartItem cartItem : cartItems){
+            cart.addCartItem(cartItem);
+        }
+
+        return cart;
+    }
+
+
+
 }
---
+++ src/main/java/zoas/demo/domian/CartItem.java
@@ -0,0 +1,40 @@
+package zoas.demo.domian;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem {
+
+    @Id @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+
+    private int cartPrice;
+
+    private int count;
+
+
+
+    //비지니스 로직
+    public int getTotalPrice(){
+        return getCartPrice() * getCount();
+    }
+
+
+}
